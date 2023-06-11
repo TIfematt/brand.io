52c378d72772c70c58c89c2948a3f7dcb07db9ce
Boluwatife <Boluwatifeoladejo@gmail.com>
2023-06-11 16:07:14 +0100

    Mobile navbar added

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -9,7 +9,7 @@ import { motion } from 'framer-motion';
 
 const Nav = () => {
 
-    const [toggle, setToggle] = useState(true)
+    const [toggle, setToggle] = useState(false)
 
   return (
     <nav className=" mx-10 my-5 flex justify-between sm:mx-5 sm:my-9 sm:justify-around sm:items-center"> 
@@ -34,19 +34,19 @@ const Nav = () => {
 
         {/* mobile Navigation */}
         <div className="sm:hidden flex items-center h-full ">
-            <GiHamburgerMenu  onClick={() => setToggle(true) } />
+            <GiHamburgerMenu className="w-12 h-12" onClick={() => setToggle(true) } />
             {toggle && (
                 <motion.div
                     whileInView={{ x: [300, 0] }}
                     transition={{ duration: 0.85, ease: 'easeOut' }}
                     className="nav-mobile bg-grey-600"
                 >
-                    <GrClose onClick={() => setToggle(false)} />
+                    <GrClose onClick={() => setToggle(false)} className="nav-close"/>
                     <ul className="flex flex-col gap-y-7 -ml-1">
-                        <li><Link href='/' className="nav-link-home">Home</Link></li>
-                        <li><Link href='/about' className="nav-link">About-Us</Link></li>
-                        <li><Link href='/services' className="nav-link">Services</Link></li>
-                        <li><Link href='/contact' className="nav-link">Contact-Us</Link></li>
+                        <li className=""><Link href='/' className="nav-link-home font-bold">Home</Link></li>
+                        <li><Link href='/about' className="nav-link font-bold">About-Us</Link></li>
+                        <li><Link href='/services' className="nav-link font-bold">Services</Link></li>
+                        <li><Link href='/contact' className="nav-link font-bold">Contact-Us</Link></li>
                     </ul>
                     <button className="btn w-24 h-12 rounded-lg text-white font-bold flex items-center justify-center">
                         Login
