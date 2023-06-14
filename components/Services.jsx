c326d972942687b50a96adae1b7e0d528557e1b0
Boluwatife <Boluwatifeoladejo@gmail.com>
2023-06-14 15:32:33 +0100

    done with service section

--- components/Services.jsx
+++ components/Services.jsx
@@ -2,31 +2,128 @@ import React from 'react'
 import Image from 'next/image'
 
 const Services = () => {
+
+    // const imageStyle = {
+    //     width: '370px',
+    //     height: '270px',
+    //     position: 'relative',
+    //     top: '6px'
+
+    // }
+
   return (
     <div>
         <section className='flex flex-col items-center justify-center gap-7'>
             <h1 className='hero-heading text-4xl sm:text-6xl font-semibold font-inter'>What we Offer?</h1>
-            <p className='max-w-2xl text-center'> In virtual space through communication platforms. 
-                Durable relations that extend beyond immediate 
+            <p className='max-w-2xl text-center text-primary filter drop-shadow-lg'> In virtual space through communication platforms.
+                Durable relations that extend beyond immediate
                 genealogical ties.
             </p>
         </section>
         {/* cards */}
-        <section className='mt-8'>
-            <div className='px-4 w-64 h-60 bg-white border-lg shadow-sm'>
-                <div className=' bg-gray-400 bg-opacity-25 filter drop-shadow-lg rounded-full w-12 h-12 flex items-center justify-center'>
-                    <div className='message-icon' />
-                    {/* <Image 
-                        src='/assets/messageIcon.png'
-                        width={120}
-                        height={120}
-                        alt='logo'
-                    /> */}
+        <section className='mt-16 flex flex-col gap-12 sm:flex  sm:gap-12 sm:flex-col items-center justify-center'>
+            {/* row one */}
+            <div className='flex flex-col sm:flex-row gap-12 sm:flex sm:gap-8'>
+                <div className='px-4 w-64 h-60 bg-white border-lg shadow-sm'>
+                    <div className='relative z-20 bg-gray-400 bg-opacity-25 filter drop-shadow-lg rounded-full w-12 h-12 flex items-center justify-center'>
+                        {/* <div className='message-icon z-30' /> */}
+                        {/* <Image
+                            src='/assets/messageIcon.png'
+                            style={imageStyle}
+                            width={180}
+                            height={120}
+                            alt='logo'
+                        /> */}
+                    </div>
+                    <h3 className='pt-6 text-lg font-bold font-inter'>Community Support</h3>
+                    <p className='pt-2 max-w-xm text-primary font-satoshi filter drop-shadow-lg'>
+                        Apps keep work and information right at your fingertips—and
+                        Slack keeps it all securely.
+                    </p>
                 </div>
-                <h3 className='pt-6 text-lg font-bold font-inter'>Community Support</h3>
-                <p className='pt-2 max-w-xm text-primary font-satoshi'>Apps keep work and information right at your fingertips—and 
-                    Slack keeps it all securely.
-                </p>
+                <div className='px-4 w-64 h-60 bg-white border-lg shadow-sm'>
+                    <div className='relative z-20 bg-gray-400 bg-opacity-25 filter drop-shadow-lg rounded-full w-12 h-12 flex items-center justify-center'>
+                        {/* <div className='message-icon z-30' /> */}
+                        {/* <Image
+                            src='/assets/messageIcon.png'
+                            style={imageStyle}
+                            width={180}
+                            height={120}
+                            alt='logo'
+                        /> */}
+                    </div>
+                    <h3 className='pt-6 text-lg font-bold font-inter'>Security Firstt</h3>
+                    <p className='pt-2 max-w-xm text-primary font-satoshi filter drop-shadow-lg'>
+                        Connect the tools you already use to get more from them every time you work.
+                    </p>
+                </div>
+                <div className='px-4 w-64 h-60 bg-white border-lg shadow-sm'>
+                    <div className='relative z-20 bg-gray-400 bg-opacity-25 filter drop-shadow-lg rounded-full w-12 h-12 flex items-center justify-center'>
+                        {/* <div className='message-icon z-30' /> */}
+                        {/* <Image
+                            src='/assets/messageIcon.png'
+                            style={imageStyle}
+                            width={180}
+                            height={120}
+                            alt='logo'
+                        /> */}
+                    </div>
+                    <h3 className='pt-6 text-lg font-bold font-inter'>24/7 Admin Support</h3>
+                    <p className='pt-2 max-w-xm text-primary font-satoshi filter drop-shadow-lg'>
+                        You can work with apps in channels, where they’re a seamless part of the conver.
+                    </p>
+                </div>
+            </div>
+            {/* row two */}
+            <div className='flex flex-col sm:flex-row gap-12 sm:flex sm:gap-8'>
+                <div className='px-4 w-64 h-60 bg-white border-lg shadow-sm'>
+                    <div className='relative z-20 bg-gray-400 bg-opacity-25 filter drop-shadow-lg rounded-full w-12 h-12 flex items-center justify-center'>
+                        {/* <div className='message-icon z-30' /> */}
+                        {/* <Image
+                            src='/assets/messageIcon.png'
+                            style={imageStyle}
+                            width={180}
+                            height={120}
+                            alt='logo'
+                        /> */}
+                    </div>
+                    <h3 className='pt-6 text-lg font-bold font-inter'>100+ Themes</h3>
+                    <p className='pt-2 max-w-xm text-primary font-satoshi filter drop-shadow-lg'>
+                        Spend less time tracking down the people needed to get work done.
+                    </p>
+                </div>
+                <div className='px-4 w-64 h-60 bg-white border-lg shadow-sm'>
+                    <div className='relative z-20 bg-gray-400 bg-opacity-25 filter drop-shadow-lg rounded-full w-12 h-12 flex items-center justify-center'>
+                        {/* <div className='message-icon z-30' /> */}
+                        {/* <Image
+                            src='/assets/messageIcon.png'
+                            style={imageStyle}
+                            width={180}
+                            height={120}
+                            alt='logo'
+                        /> */}
+                    </div>
+                    <h3 className='pt-6 text-lg font-bold font-inter'>Fully Integrated</h3>
+                    <p className='pt-2 max-w-xm text-primary font-satoshi filter drop-shadow-lg'>
+                        Skip the follow-ups and back-and -forth. Easily standardize how you collect 
+                    </p>
+                </div>
+                <div className='px-4 w-64 h-60 bg-white border-lg shadow-sm'>
+                    <div className='relative z-20 bg-gray-400 bg-opacity-25 filter drop-shadow-lg rounded-full w-12 h-12 flex items-center justify-center'>
+                        {/* <div className='message-icon z-30' /> */}
+                        {/* <Image
+                            src='/assets/messageIcon.png'
+                            style={imageStyle}
+                            width={180}
+                            height={120}
+                            alt='logo'
+                        /> */}
+                    </div>
+                    <h3 className='pt-6 text-lg font-bold font-inter'>Monthly Update</h3>
+                    <p className='pt-2 max-w-xm text-primary font-satoshi filter drop-shadow-lg'>
+                        Take control of your work day. In a just a few minutes, create a customized workflow 
+                    </p>
+                </div>
             </div>
         </section>
 
